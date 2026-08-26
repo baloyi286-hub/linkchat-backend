2e61f959f5e981e88cdb17a9f97935f88a23ff8d
Nkhenso Baloyi <baloyi286@gmail.com>
2026-08-26 08:18:55 +0200

    updated security config

--- src/main/java/com/linkchat/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/linkchat/infrastructure/config/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource(@Value("${app.frontend-url}") String frontend) {
         var c = new CorsConfiguration();
-        c.setAllowedOrigins(List.of(frontend));
+        c.setAllowedOrigins(List.of("http://localhost:5173",frontend));
         c.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         c.setAllowedHeaders(List.of("*"));
         c.setAllowCredentials(true);
