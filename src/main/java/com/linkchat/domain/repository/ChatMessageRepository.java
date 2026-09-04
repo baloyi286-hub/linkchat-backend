54803f47edba7195317482c3ee90ec81d9b4c8af
baloyi286-hub <baloyi286@gmail.com>
2026-09-04 23:53:51 +0200

    Support deleting conversation messages

--- src/main/java/com/linkchat/domain/repository/ChatMessageRepository.java
+++ src/main/java/com/linkchat/domain/repository/ChatMessageRepository.java
@@ -1,1 +1,7 @@
-package com.linkchat.domain.repository; import com.linkchat.domain.model.ChatMessage; import java.util.*; public interface ChatMessageRepository { ChatMessage save(ChatMessage m); List<ChatMessage> findByConversationIdOrderByCreatedAtAsc(UUID conversationId); }
+package com.linkchat.domain.repository;
+import com.linkchat.domain.model.ChatMessage; import java.util.*;
+public interface ChatMessageRepository {
+    ChatMessage save(ChatMessage m);
+    List<ChatMessage> findByConversationIdOrderByCreatedAtAsc(UUID conversationId);
+    void deleteByConversationId(UUID conversationId);
+}
