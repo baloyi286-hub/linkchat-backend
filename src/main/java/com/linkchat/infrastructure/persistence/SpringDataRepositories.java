43b7ea91cd7cd12cb91b2c1a815a2b13103d5d5f
baloyi286-hub <baloyi286@gmail.com>
2026-09-04 23:53:57 +0200

    Add message deletion persistence method

--- src/main/java/com/linkchat/infrastructure/persistence/SpringDataRepositories.java
+++ src/main/java/com/linkchat/infrastructure/persistence/SpringDataRepositories.java
@@ -8,20 +8,7 @@ interface AccountJpaRepository extends JpaRepository<Account, UUID> {
     Optional<Account> findByInviteCode(String inviteCode);
     Optional<Account> findByAuthSubject(String authSubject);
 }
-
-interface VisitorJpaRepository extends JpaRepository<VisitorProfile, UUID> {
-    Optional<VisitorProfile> findByBrowserTokenHash(String hash);
-}
-
-interface VisitorImageJpaRepository extends JpaRepository<VisitorImage, UUID> {
-    List<VisitorImage> findByVisitorId(UUID visitorId);
-    void deleteByVisitorId(UUID visitorId);
-}
-
-interface ConversationJpaRepository extends JpaRepository<Conversation, UUID> {
-    List<Conversation> findByOwnerIdOrderByCreatedAtDesc(UUID ownerId);
-}
-
-interface ChatMessageJpaRepository extends JpaRepository<ChatMessage, UUID> {
-    List<ChatMessage> findByConversationIdOrderByCreatedAtAsc(UUID conversationId);
-}
+interface VisitorJpaRepository extends JpaRepository<VisitorProfile, UUID> { Optional<VisitorProfile> findByBrowserTokenHash(String hash); }
+interface VisitorImageJpaRepository extends JpaRepository<VisitorImage, UUID> { List<VisitorImage> findByVisitorId(UUID visitorId); void deleteByVisitorId(UUID visitorId); }
+interface ConversationJpaRepository extends JpaRepository<Conversation, UUID> { List<Conversation> findByOwnerIdOrderByCreatedAtDesc(UUID ownerId); }
+interface ChatMessageJpaRepository extends JpaRepository<ChatMessage, UUID> { List<ChatMessage> findByConversationIdOrderByCreatedAtAsc(UUID conversationId); void deleteByConversationId(UUID conversationId); }
